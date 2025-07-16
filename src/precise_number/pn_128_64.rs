84974ecaf8d16e4bf212841a932b2f7493197d48
GroovieGermanikus <grooviegermanikus@gmail.com>
2025-07-16 16:40:11 +0200

    refactor pn and add more precisions

---
+++ src/precise_number/pn_128_64.rs
@@ -0,0 +1,44 @@
+use crate::define_precise_number;
+
+const ONE_CONST: u128 = 1_000_000_000;
+const ROUNDING_CORRECTION: u128 = 1_000_000_000 / 2;
+const PRECISION: u128 = 10; // TODO
+const MAXIMUM_SQRT_BASE: u128 = u64::MAX as u128 * ONE_CONST;
+define_precise_number!(
+    PreciseNumber,
+    u64,
+    u128,
+    ONE_CONST,
+    0u128,
+    ROUNDING_CORRECTION,
+    PRECISION,
+    MAXIMUM_SQRT_BASE
+);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_u128_max_outer_to_precise() {
+        let a = PreciseNumber::new(u64::MAX);
+        assert_eq!(a.to_imprecise().unwrap(), u64::MAX);
+        let a_plus_1 = a.checked_add(&PreciseNumber::one()).unwrap();
+        assert!(a_plus_1.to_imprecise().is_none());
+    }
+
+    #[test]
+    fn test_u128_one_constant() {
+        assert_eq!(ONE_CONST, 1_000_000_000);
+    }
+
+    #[test]
+    fn test_u128_rounding_correction_constant() {
+        assert_eq!(ROUNDING_CORRECTION, 500_000_000);
+    }
+
+    #[test]
+    fn test_u128_maximum_sqrt_base_constant() {
+        assert_eq!(MAXIMUM_SQRT_BASE, PreciseNumber::new(u64::MAX).value);
+    }
+}
---
+++ src/precise_number/pn_tests.rs
@@ -0,0 +1,341 @@
+#[cfg(test)]
+mod tests {
+    use crate::define_precise_number;
+    use crate::uint::U256;
+    use proptest::prelude::*;
+
+    type InnerUint = U256;
+
+    const ONE_CONST: U256 = U256([1000000000000, 0, 0, 0]);
+    const ROUNDING_CORRECTION: U256 = U256([1000000000000 / 2, 0, 0, 0]);
+    const PRECISION: U256 = U256([100, 0, 0, 0]);
+    const MAXIMUM_SQRT_BASE: U256 =
+        U256([18446743073709551616, 18446744073709551615, 999999999999, 0]); // u128::MAX
+    define_precise_number!(
+        PreciseNumber,
+        u128,
+        U256,
+        ONE_CONST,
+        U256::zero(),
+        ROUNDING_CORRECTION,
+        PRECISION,
+        MAXIMUM_SQRT_BASE
+    );
+
+    define_precise_number!(TestPreciseNumber8, u8, u8, 10u8, 0u8, 5u8, 1u8, 10u8);
+    define_precise_number!(
+        TestPreciseNumber32,
+        u32,
+        u32,
+        1_000u32,
+        0u32,
+        500u32,
+        1u32,
+        1_000u32
+    ); // MAXIMUM_SQRT_BASE is likely incorrect
+
+    fn check_pow_approximation(base: InnerUint, exponent: InnerUint, expected: InnerUint) {
+        let precision = InnerUint::from(5_000_000); // correct to at least 3 decimal places
+        let base = PreciseNumber { value: base };
+        let exponent = PreciseNumber { value: exponent };
+        let root = base
+            .checked_pow_approximation(&exponent, PreciseNumber::MAX_APPROXIMATION_ITERATIONS)
+            .unwrap();
+        let expected = PreciseNumber { value: expected };
+        assert!(root.almost_eq(&expected, precision));
+    }
+
+    #[test]
+    fn test_max_number_to_u128() {
+        // 3.4e38
+        // let a = PreciseNumber::new(300282366920938463463374607431768211455).unwrap();
+        // let b = PreciseNumber::new(300282366920938463463374607431768211455).unwrap();
+
+        let a = PreciseNumber::new(3.4e32 as u128);
+        let b = PreciseNumber::new(3.4e32 as u128);
+        // max 3,4028236692×10³²
+
+        a.checked_mul(&b).unwrap();
+    }
+
+    #[test]
+    fn test_max_int_val() {
+        // 2^32 / 1000 // 4294967296 / 1000 = 4294967.296
+        let _ = TestPreciseNumber32::new(4294967);
+    }
+
+    #[test]
+    fn test_to_imprecise_rounding() {
+        fn calc(a: u8, b: u8) -> u8 {
+            let a = TestPreciseNumber8::new(a);
+            // println!("a: {}", a.value);
+            let b = TestPreciseNumber8::new(b);
+            // println!("b: {}", b.value);
+            let c = a.checked_div(&b).unwrap();
+            // println!("c: {}", c.value);
+            c.to_imprecise().unwrap()
+        }
+
+        // rounding mode HALF_DOWN
+        assert_eq!(calc(11, 2), 5);
+        assert_eq!(calc(5, 2), 2);
+        assert_eq!(calc(4, 3), 1);
+    }
+
+    #[test]
+    fn test_root_approximation() {
+        let one = PreciseNumber::FP_ONE;
+        // square root
+        check_pow_approximation(one / 4, one / 2, one / 2); // 1/2
+        check_pow_approximation(one * 11 / 10, one / 2, InnerUint::from(1_048808848161u128)); // 1.048808848161
+
+        // 5th root
+        check_pow_approximation(one * 4 / 5, one * 2 / 5, InnerUint::from(914610103850u128));
+        // 0.91461010385
+
+        // 10th root
+        check_pow_approximation(one / 2, one * 4 / 50, InnerUint::from(946057646730u128));
+        // 0.94605764673
+    }
+
+    fn check_pow_fraction(
+        base: InnerUint,
+        exponent: InnerUint,
+        expected: InnerUint,
+        precision: InnerUint,
+    ) {
+        let base = PreciseNumber { value: base };
+        let exponent = PreciseNumber { value: exponent };
+        let power = base.checked_pow_fraction(&exponent).unwrap();
+        let expected = PreciseNumber { value: expected };
+        assert!(power.almost_eq(&expected, precision));
+    }
+
+    #[test]
+    fn test_pow_fraction() {
+        let one = PreciseNumber::FP_ONE;
+        let precision = InnerUint::from(50_000_000); // correct to at least 3 decimal places
+        let less_precision = precision * 1_000; // correct to at least 1 decimal place
+        check_pow_fraction(one, one, one, precision);
+        check_pow_fraction(
+            one * 20 / 13,
+            one * 50 / 3,
+            InnerUint::from(1312_534484739100u128),
+            precision,
+        ); // 1312.5344847391
+        check_pow_fraction(one * 2 / 7, one * 49 / 4, InnerUint::from(2163), precision);
+        check_pow_fraction(
+            one * 5000 / 5100,
+            one / 9,
+            InnerUint::from(997802126900u128),
+            precision,
+        ); // 0.99780212695
+           // results get less accurate as the base gets further from 1, so allow
+           // for a greater margin of error
+        check_pow_fraction(
+            one * 2,
+            one * 27 / 5,
+            InnerUint::from(42_224253144700u128),
+            less_precision,
+        ); // 42.2242531447
+        check_pow_fraction(
+            one * 18 / 10,
+            one * 11 / 3,
+            InnerUint::from(8_629769290500u128),
+            less_precision,
+        ); // 8.629769290
+    }
+
+    #[test]
+    fn test_newtonian_approximation() {
+        let test = PreciseNumber::new(0);
+        let nth_root = PreciseNumber::new(0);
+        let guess = test.checked_div(&nth_root);
+        assert_eq!(guess, Option::None);
+
+        // square root
+        let test = PreciseNumber::new(9);
+        let nth_root = PreciseNumber::new(2);
+        let guess = test.checked_div(&nth_root).unwrap();
+        let root = test
+            .newtonian_root_approximation(
+                &nth_root,
+                guess,
+                PreciseNumber::MAX_APPROXIMATION_ITERATIONS,
+            )
+            .unwrap()
+            .to_imprecise()
+            .unwrap();
+        assert_eq!(root, 3); // actually 3
+
+        let test = PreciseNumber::new(101);
+        let nth_root = PreciseNumber::new(2);
+        let guess = test.checked_div(&nth_root).unwrap();
+        let root = test
+            .newtonian_root_approximation(
+                &nth_root,
+                guess,
+                PreciseNumber::MAX_APPROXIMATION_ITERATIONS,
+            )
+            .unwrap()
+            .to_imprecise()
+            .unwrap();
+        assert_eq!(root, 10); // actually 10.049875
+
+        let test = PreciseNumber::new(1_000_000_000);
+        let nth_root = PreciseNumber::new(2);
+        let guess = test.checked_div(&nth_root).unwrap();
+        let root = test
+            .newtonian_root_approximation(
+                &nth_root,
+                guess,
+                PreciseNumber::MAX_APPROXIMATION_ITERATIONS,
+            )
+            .unwrap()
+            .to_imprecise()
+            .unwrap();
+        assert_eq!(root, 31_623); // actually 31622.7766
+
+        // 5th root
+        let test = PreciseNumber::new(500);
+        let nth_root = PreciseNumber::new(5);
+        let guess = test.checked_div(&nth_root).unwrap();
+        let root = test
+            .newtonian_root_approximation(
+                &nth_root,
+                guess,
+                PreciseNumber::MAX_APPROXIMATION_ITERATIONS,
+            )
+            .unwrap()
+            .to_imprecise()
+            .unwrap();
+        assert_eq!(root, 3); // actually 3.46572422
+    }
+
+    #[test]
+    fn test_checked_div() {
+        let one_tenth = PreciseNumber::new(1)
+            .checked_div(&PreciseNumber::new(10))
+            .unwrap();
+        let two = PreciseNumber::new(2);
+        let c = one_tenth.checked_div(&one_tenth).unwrap();
+        let e = PreciseNumber::new(1).checked_div(&c).unwrap();
+        let d = c.checked_mul(&two).unwrap();
+        assert_eq!(e.to_imprecise().unwrap(), 1);
+        assert_eq!(d.to_imprecise().unwrap(), 2);
+    }
+
+    #[test]
+    fn test_checked_mul() {
+        let number_one = PreciseNumber::new(0);
+        let number_two = PreciseNumber::new(0);
+        let result = number_one.checked_mul(&number_two);
+        assert_eq!(
+            result,
+            Option::Some(PreciseNumber {
+                value: U256::from(0)
+            })
+        );
+
+        let number_one = PreciseNumber::new(2);
+        let number_two = PreciseNumber::new(2);
+        let result = number_one.checked_mul(&number_two).unwrap();
+        assert_eq!(result, PreciseNumber::new(2 * 2));
+
+        let number_one = PreciseNumber { value: U256::MAX };
+        let number_two = PreciseNumber::new(1);
+        let result = number_one.checked_mul(&number_two).unwrap();
+        assert_eq!(
+            result.value,
+            U256::MAX / PreciseNumber::FP_ONE * PreciseNumber::FP_ONE
+        );
+
+        let number_one = PreciseNumber { value: U256::MAX };
+        let mut number_two = PreciseNumber::new(1);
+        number_two.value += U256::from(1);
+        let result = number_one.checked_mul(&number_two);
+        assert_eq!(result, Option::None);
+    }
+
+    fn check_square_root(check: &PreciseNumber) {
+        let epsilon = PreciseNumber {
+            value: InnerUint::from(10),
+        }; // correct within 11 decimals
+        let one = PreciseNumber::one();
+        let one_plus_epsilon = one.checked_add(&epsilon).unwrap();
+        let one_minus_epsilon = one.checked_sub(&epsilon).unwrap();
+        let approximate_root = check.sqrt().unwrap();
+        let lower_bound = approximate_root
+            .checked_mul(&one_minus_epsilon)
+            .unwrap()
+            .checked_pow(2)
+            .unwrap();
+        let upper_bound = approximate_root
+            .checked_mul(&one_plus_epsilon)
+            .unwrap()
+            .checked_pow(2)
+            .unwrap();
+        assert!(check.less_than_or_equal(&upper_bound));
+        assert!(check.greater_than_or_equal(&lower_bound));
+    }
+
+    #[test]
+    fn test_square_root_min_max() {
+        let test_roots = [
+            PreciseNumber::minimum_sqrt_base(),
+            PreciseNumber::maximum_sqrt_base(),
+        ];
+        for i in test_roots.iter() {
+            check_square_root(i);
+        }
+    }
+
+    #[test]
+    fn test_floor() {
+        let whole_number = PreciseNumber::new(2);
+        let mut decimal_number = PreciseNumber::new(2);
+        decimal_number.value += InnerUint::from(1);
+        let floor = decimal_number.floor().unwrap();
+        let floor_again = floor.floor().unwrap();
+        assert_eq!(whole_number.value, floor.value);
+        assert_eq!(whole_number.value, floor_again.value);
+    }
+
+    #[test]
+    fn test_ceiling() {
+        // 1.999999999999
+        let mut decimal_number = PreciseNumber::new(2);
+        decimal_number.value -= InnerUint::from(1);
+        let ceiling = decimal_number.ceiling().unwrap();
+        let ceiling_again = ceiling.ceiling().unwrap();
+
+        let expected_fp2: InnerUint = PreciseNumber::new(2).value;
+        assert_eq!(ceiling.value, expected_fp2);
+        assert_eq!(ceiling_again.value, expected_fp2);
+    }
+
+    #[test]
+    fn test_ceiling_all() {
+        for value in 0..=246 {
+            let a = TestPreciseNumber8 { value };
+
+            let ceil_expected = (value as f64 / 10.0).ceil() as u8;
+            let ceiling = a.ceiling().unwrap();
+
+            assert_eq!(ceiling.value, ceil_expected * 10);
+        }
+
+        for value in 247..=255 {
+            let a = TestPreciseNumber8 { value };
+            assert!(a.ceiling().is_none(), "will overflow");
+        }
+    }
+
+    proptest! {
+        #[test]
+        fn test_square_root(a in 0..u128::MAX) {
+            let a = PreciseNumber { value: InnerUint::from(a) };
+            check_square_root(&a);
+        }
+    }
+}
