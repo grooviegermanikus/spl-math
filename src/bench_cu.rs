e4f3a1fa18c398bad7bf30950f6fc080682b985e
GroovieGermanikus <grooviegermanikus@gmail.com>
2025-07-16 13:10:14 +0200

    basic mollusk

---
+++ src/bench_cu.rs
@@ -0,0 +1,44 @@
+use solana_instruction::{AccountMeta, Instruction};
+use {
+    mollusk_svm_bencher::MolluskComputeUnitBencher,
+    mollusk_svm::Mollusk,
+    /* ... */
+};
+
+#[test]
+fn cu_bench() {
+
+    // Optionally disable logging.
+    solana_logger::setup_with("");
+
+    /* Instruction & accounts setup ... */
+
+    let program_id = solana_pubkey::Pubkey::new_unique();
+
+    let mollusk = Mollusk::new(&program_id, "counter");
+
+
+    let instruction = Instruction::new_with_bytes(
+        program_id,
+        &[],
+        vec![
+            // AccountMeta::new(key1, false),
+            // AccountMeta::new_readonly(key2, false),
+        ],
+    );
+
+    let accounts = vec![
+        // (key1, Account::default()),
+        // (key2, Account::default()),
+    ];
+
+
+
+    MolluskComputeUnitBencher::new(mollusk)
+        .bench(("bench0", &instruction, &accounts))
+        .must_pass(false)
+        .out_dir("../target/benches")
+        .execute();
+
+
+}
