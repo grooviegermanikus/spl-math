a29da8b1a1ac94d6ee6454623d53307a5f3e8342
GroovieGermanikus <grooviegermanikus@gmail.com>
2025-07-21 13:24:16 +0200

    build testdata eagerly

--- benches/benches_precision_number.rs
+++ benches/benches_precision_number.rs
@@ -1,11 +1,14 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use spl_math::PreciseNumber;
 
 mod pn;
 
 criterion_group!(
     benches_basic_math,
     pn::basic_math::bench_add,
+    pn::basic_math::bench_sub,
+    pn::basic_math::bench_ceiling,
+    pn::basic_math::bench_mul,
+    pn::basic_math::bench_div,
 );
 
 criterion_main!(benches_basic_math);
--- benches/pn/basic_math.rs
+++ benches/pn/basic_math.rs
@@ -1,24 +1,125 @@
+use std::ops::Sub;
 use criterion::Criterion;
 use itertools::Itertools;
 use spl_math::PreciseNumber;
 use spl_math::uint::U256;
 
 pub(crate) fn bench_add(c: &mut Criterion) {
+    const SAMPLES: u64 = 1_000_000;
 
-    const SAMPLES: usize = 1_000_000;
+    let testdata = (0..SAMPLES)
+        .map(|i| i).collect_vec();
 
-    // note: must be infinite
-    let mut testdata_iter = (0..SAMPLES)
-        .map(|i| i).cycle();
+    let mut testdata_iter = testdata.into_iter().cycle();
 
     c.bench_function("bench_add", |b| {
         b.iter(|| {
             let i = testdata_iter.next()?;
-            let a = PreciseNumber { value: U256::from(i as u64) };
-            let b = PreciseNumber { value: U256::from(1_000_000_000_000u64) };
+            let a = PreciseNumber { value: U256::from(i) };
+            let b = PreciseNumber { value: U256::from(1_000_000_000_000u64.sub(i as u64)) };
             let result = a.checked_add(&b).unwrap();
 
             Some(result)
         });
     });
 }
+
+pub(crate) fn bench_sub(c: &mut Criterion) {
+    const SAMPLES: u64 = 1_000_000;
+
+    let testdata = (0..SAMPLES)
+        .map(|i| i).collect_vec();
+
+    let mut testdata_iter = testdata.into_iter().cycle();
+
+    c.bench_function("bench_sub", |b| {
+        b.iter(|| {
+            let i = testdata_iter.next()?;
+            let a = PreciseNumber { value: U256::from(1_000_000_000_000u64.sub(i as u64)) };
+            let b = PreciseNumber { value: U256::from(i as u64) };
+            let result = a.checked_sub(&b).unwrap();
+
+            Some(result)
+        });
+    });
+}
+
+pub(crate) fn bench_ceiling(c: &mut Criterion) {
+    const SAMPLES: u64 = 1_000_000;
+
+    let testdata = (1..=SAMPLES)
+        .map(|i| PreciseNumber::new(1)
+            .checked_add(&PreciseNumber {
+                value: U256::from(i as u64)
+            }).unwrap()).collect_vec();
+
+    let mut testdata_iter = testdata.into_iter().cycle();
+
+    c.bench_function("bench_ceiling", |b| {
+        b.iter(|| {
+            let a = testdata_iter.next()?;
+            let result = a.ceiling().unwrap();
+
+            Some(result)
+        });
+    });
+}
+
+
+pub(crate) fn bench_mul(c: &mut Criterion) {
+    const SAMPLES: u64 = 1_000_000;
+
+    let testdata = (1..=SAMPLES)
+        .map(|i| (
+            PreciseNumber::new(100)
+                .checked_add(&PreciseNumber {
+                    value: U256::from(i as u64)
+                }).unwrap(),
+            PreciseNumber::new(200)
+                .checked_add(&PreciseNumber {
+                    value: U256::from(i * 3)
+                }).unwrap(),
+        )).collect_vec();
+
+    let mut testdata_iter = testdata.into_iter().cycle();
+
+    c.bench_function("bench_mul", |b| {
+        b.iter(|| {
+            let (a, b) = testdata_iter.next()?;
+            let result = a.checked_mul(&b).unwrap();
+
+            Some(result)
+        });
+    });
+}
+
+
+
+pub(crate) fn bench_div(c: &mut Criterion) {
+    const SAMPLES: u64 = 1_000_000;
+
+    let testdata = (1..=SAMPLES)
+        .map(|i| (
+            PreciseNumber::new(if i % 2 == 0 { 100 } else { u128::MAX / 1_000_000 })
+                .checked_add(&PreciseNumber {
+                    value: U256::from(i as u64)
+                }).unwrap(),
+            PreciseNumber::new(200)
+                .checked_add(&PreciseNumber {
+                    value: U256::from(i * 3)
+                }).unwrap(),
+        )).collect_vec();
+
+    let mut testdata_iter = testdata.into_iter().cycle();
+
+    c.bench_function("bench_div", |b| {
+        b.iter(|| {
+            let (a, b) = testdata_iter.next()?;
+            let result = a.checked_div(&b).unwrap();
+
+            Some(result)
+        });
+    });
+}
+
+
