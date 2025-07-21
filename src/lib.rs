93ecc9f69638991b4005f2c7d3555ed081091517
GroovieGermanikus <grooviegermanikus@gmail.com>
2025-07-21 15:45:42 +0200

    split types

--- src/lib.rs
+++ src/lib.rs
@@ -8,5 +8,7 @@ pub mod uint;
 
 // map precise number implementation
 mod precise_number;
-pub type PreciseNumber = precise_number::pn_256_128::PreciseNumber;
-pub type PreciseNumber128 = precise_number::pn_128_64::PreciseNumber;
+// type aliases for initial version of PreciseNumber
+pub type PreciseNumber = precise_number::pn_256_128_d12::PreciseNumber;
+pub type PreciseNumber128D12 = precise_number::pn_128_64_d12::PreciseNumber;
+pub type PreciseNumber256D18 = precise_number::pn_256_128_d18::PreciseNumber;
