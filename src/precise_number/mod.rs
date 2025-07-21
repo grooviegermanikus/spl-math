460853d6531ce347ff9e7d5436e518e3953fba36
GroovieGermanikus <grooviegermanikus@gmail.com>
2025-07-21 15:45:42 +0200

    split types

--- src/precise_number/mod.rs
+++ src/precise_number/mod.rs
@@ -1,5 +1,6 @@
-pub(crate) mod pn_128_64;
-pub(crate) mod pn_256_128;
-pub(crate) mod pn_64_32;
+pub(crate) mod pn_128_64_d12;
+pub(crate) mod pn_256_128_d12;
+pub(crate) mod pn_256_128_d18;
+pub(crate) mod pn_64_32_d12;
 mod pn_impl;
 mod pn_tests;
