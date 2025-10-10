cbd43ca5efa7fde19426a3f4f5a75726a8cab41f
GroovieGermanikus <grooviegermanikus@gmail.com>
2025-10-10 19:14:30 +0200

    expand macro

--- src/precise_number/pn_muldiv_tests.rs
+++ src/precise_number/pn_muldiv_tests.rs
@@ -13,8 +13,16 @@ mod tests {
         let c = TestPreciseNumber8 { value: 25 };
 
         // (10 * 5) / 2 = 25
-        let result = a.mul_div_floor(b, c).unwrap();
+        let result = mul_div_floor(a, b, c).unwrap();
         assert_eq!(result.value, 200);
     }
 
+    pub fn mul_div_floor(base: TestPreciseNumber8, num: TestPreciseNumber8, denom: TestPreciseNumber8) -> Option<TestPreciseNumber8> {
+        let r = base.value as u16 * num.value as u16
+            / denom.value as u16;
+
+        u8::try_from(r)
+            .map(|v| TestPreciseNumber8 { value: v })
+            .ok()
+    }
 }
