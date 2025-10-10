da28514f1aa14d147fd31717ae133fc1b7ee84fa
GroovieGermanikus <grooviegermanikus@gmail.com>
2025-10-10 19:37:50 +0200

    proptest for naiv muldiv

--- src/precise_number/pn_muldiv_tests.rs
+++ src/precise_number/pn_muldiv_tests.rs
@@ -1,5 +1,7 @@
 
 mod tests {
+    use proptest::proptest;
+    use super::*;
     use crate::{define_muldiv, define_precise_number};
 
     define_precise_number!(TestPreciseNumber8, u8, u8, 10u8, 0u8, 5u8, 1u8, 10u8);
@@ -13,16 +15,37 @@ mod tests {
         let c = TestPreciseNumber8 { value: 25 };
 
         // (10 * 5) / 2 = 25
-        let result = mul_div_floor(a, b, c).unwrap();
+        let result = mul_div_floor_hacking(a, b, c).unwrap();
         assert_eq!(result.value, 200);
     }
 
-    pub fn mul_div_floor(base: TestPreciseNumber8, num: TestPreciseNumber8, denom: TestPreciseNumber8) -> Option<TestPreciseNumber8> {
+    fn mul_div_floor_hacking(base: TestPreciseNumber8, num: TestPreciseNumber8, denom: TestPreciseNumber8) -> Option<TestPreciseNumber8> {
         let r = base.value as u16 * num.value as u16
             / denom.value as u16;
 
         u8::try_from(r)
             .map(|v| TestPreciseNumber8 { value: v })
             .ok()
+    }
+
+    fn mul_div_floor_naiv(base: TestPreciseNumber8, num: TestPreciseNumber8, denom: TestPreciseNumber8) -> Option<TestPreciseNumber8> {
+        let raw_muldiv = base.value as u32 * num.value as u32 / denom.value as u32;
+        u8::try_from(raw_muldiv)
+            .map(|v| TestPreciseNumber8 { value: v })
+            .ok()
+    }
+
+    proptest! {
+        #[test]
+        fn test_check_mul_div(a: u8, b: u8, c in 1..u8::MAX) {
+            let aa = TestPreciseNumber8 { value: a };
+            let bb = TestPreciseNumber8 { value: b };
+            let cc = TestPreciseNumber8 { value: c };
+            let r = mul_div_floor_hacking(aa.clone(), bb.clone(), cc.clone());
+
+            let expected = mul_div_floor_naiv(aa, bb, cc);
+            assert_eq!(r, expected);
+            // 128, b = 222, c = 1 OVERFLOW
+        }
     }
 }
