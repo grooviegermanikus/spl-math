db1c964d4fe142a4b726b7627a7e5a14cf44c95b
GroovieGermanikus <grooviegermanikus@gmail.com>
2025-10-27 21:38:57 +0100

    invariant

--- src/precise_number/pn_muldiv_tests.rs
+++ src/precise_number/pn_muldiv_tests.rs
@@ -1,5 +1,6 @@
 #[cfg(test)]
 mod tests {
+    use proptest::test_runner::TestCaseResult;
     use {proptest::prelude::*};
     use crate::{define_muldiv, define_precise_number};
 
@@ -8,7 +9,7 @@ mod tests {
 
 
     #[test]
-    fn test_call_muldiv() {
+    fn test_call_muldiv_floor() {
         let a = TestPreciseNumber8 { value: 100 };
         let b = TestPreciseNumber8 { value: 50 };
         let c = TestPreciseNumber8 { value: 25 };
@@ -16,6 +17,16 @@ mod tests {
         // (10 * 5) / 2 = 25
         let result = mul_div_floor_hacking(a, b, c).unwrap();
         assert_eq!(result.value, 200);
+    }
+
+    #[test]
+    fn test_call_muldiv_ceil() {
+        let a = TestPreciseNumber8 { value: 100 };
+        let b = TestPreciseNumber8 { value: 50 };
+        let c = TestPreciseNumber8 { value: 33 };
+
+        let result = mul_div_ceil_naiv(a, b, c).unwrap();
+        assert_eq!(result.value, 152);
     }
 
     fn mul_div_floor_hacking(base: TestPreciseNumber8, num: TestPreciseNumber8, denom: TestPreciseNumber8) -> Option<TestPreciseNumber8> {
@@ -46,6 +57,16 @@ mod tests {
             .ok()
     }
 
+    fn mul_div_ceil_naiv(base: TestPreciseNumber8, num: TestPreciseNumber8, denom: TestPreciseNumber8) -> Option<TestPreciseNumber8> {
+        if denom.value == 0 {
+            return None;
+        }
+        let raw_muldiv = (base.value as u32 * num.value as u32 + (denom.value as u32 - 1) )/ denom.value as u32; // why u32?
+        u8::try_from(raw_muldiv)
+            .map(|v| TestPreciseNumber8 { value: v })
+            .ok()
+    }
+
     proptest! {
         #[test]
         fn test_check_mul_div(a: u8, b: u8, c in 0..u8::MAX) {
@@ -57,6 +78,38 @@ mod tests {
             let expected = mul_div_floor_naiv(aa, bb, cc);
             assert_eq!(r, expected);
             // 128, b = 222, c = 1 OVERFLOW
+        }
+
+            // ceil(x) = floor(x + denom)
+            // ....x... 10 ........ 20
+            // floor(x) <= x <= ceil(x)
+            // x == floor(x) <=> x == ceil(x)
+        #[test]
+        fn test_check_mul_div_invariants(a: u8, b: u8, c in 1..u8::MAX) {
+            let aa = TestPreciseNumber8 { value: a };
+            let bb = TestPreciseNumber8 { value: b };
+            let denom = TestPreciseNumber8 { value: c };
+            let r_ceil = mul_div_ceil_naiv(aa.clone(), bb.clone(), denom.clone());
+            let r_floor = mul_div_floor_naiv(aa.clone(), bb.clone(), denom.clone());
+            let (Some(ceil_val), Some(floor_val)) = (r_ceil, r_floor) else {
+                // don't care about overflow cases
+                return TestCaseResult::Ok(());
+            };
+
+            assert!(floor_val.value <= ceil_val.value);
+            if floor_val.value == ceil_val.value {
+                // exact division
+                // floor(x) <= x <= ceil(x)
+                let raw_muldiv = (aa.value as u32 * bb.value as u32) / denom.value as u32; // TODO u16
+                assert_eq!(floor_val.value as u32, raw_muldiv);
+            } else {
+                // not exact division
+                // floor(x) < x < ceil(x)
+                let raw_muldiv = (aa.value as u32 * bb.value as u32) / denom.value as u32;
+                assert_eq!(floor_val.value as u32, raw_muldiv);
+                assert_eq!(ceil_val.value as u32, raw_muldiv + 1);
+            }
+
         }
     }
 }
