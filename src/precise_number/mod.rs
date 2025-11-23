8bc0f2e4b44566cfa65046f24c85eb92cfec5a5e
GroovieGermanikus <grooviegermanikus@gmail.com>
2025-11-23 14:02:29 +0100

    fix mod names for short types

--- src/precise_number/mod.rs
+++ src/precise_number/mod.rs
@@ -1,12 +1,12 @@
-mod pn_128_64_d12;
+mod pn_128_64_d9;
 mod pn_256_128_d12;
 mod pn_256_128_d18;
-mod pn_64_32_d12;
+mod pn_64_32_d4;
 mod pn_impl;
 mod pn_tests;
 
 // type aliases for initial version of PreciseNumber
 pub type PreciseNumber = pn_256_128_d12::PreciseNumber;
-pub type PreciseNumber128D12 = pn_128_64_d12::PreciseNumber;
+pub type PreciseNumber128D12 = pn_128_64_d9::PreciseNumber;
 pub type PreciseNumber256D18 = pn_256_128_d18::PreciseNumber;
 
