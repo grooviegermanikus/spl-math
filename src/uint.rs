33f75522c56e3d1b0a617ff2fbdd15fb245fa78c
GroovieGermanikus <grooviegermanikus@gmail.com>
2025-11-23 19:33:46 +0100

    cleanup

--- src/uint.rs
+++ src/uint.rs
@@ -15,20 +15,3 @@ construct_uint! {
 construct_uint! {
     pub struct U192(3);
 }
-
-impl TryFrom<f64> for U256 {
-    type Error = ();
-
-    fn try_from(value: f64) -> Result<Self, Self::Error> {
-        // let bytes: &[u64] = value.as_ref();
-        // let len = bytes.len();
-        // let lower = bytes[0..4].to_vec();
-        // let upper = bytes[4..len].to_vec();
-        // if upper.iter().any(|&x| x != 0) {
-        //     return Err(());
-        // }
-        // let u256 = U256(lower.try_into().unwrap());
-        // Ok(u256)
-        todo!()
-    }
-}
