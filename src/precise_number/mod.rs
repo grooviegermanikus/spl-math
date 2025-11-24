9b6987ebd1cb29bfefb7ed7cf9eaf311e8263717
GroovieGermanikus <grooviegermanikus@gmail.com>
2025-11-24 19:23:08 +0100

    fix name pn_128_64_d9 -> PreciseNumber128D9

--- src/precise_number/mod.rs
+++ src/precise_number/mod.rs
@@ -7,6 +7,6 @@ mod pn_tests;
 
 // type aliases for initial version of PreciseNumber
 pub type PreciseNumber = pn_256_128_d12::PreciseNumber;
-pub type PreciseNumber128D12 = pn_128_64_d9::PreciseNumber;
+pub type PreciseNumber128D9 = pn_128_64_d9::PreciseNumber;
 pub type PreciseNumber256D18 = pn_256_128_d18::PreciseNumber;
 
