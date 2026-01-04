750aef8f4b4a1a4f322281e2e3161e2b144c6527
GroovieGermanikus <grooviegermanikus@gmail.com>
2026-01-04 14:46:22 +0100

    small tweaks in sqrt_binary_system

--- src/sqrt_math.rs
+++ src/sqrt_math.rs
@@ -1,6 +1,7 @@
 #![allow(clippy::arithmetic_side_effects)]
 //! Approximation calculations
 
+use num_traits::Zero;
 use {
     num_traits::{CheckedShl, CheckedShr, PrimInt},
     std::cmp::Ordering,
@@ -24,16 +25,16 @@ pub fn sqrt_binary_system<T: PrimInt + CheckedShl + CheckedShr>(radicand: T) -> 
 
     // Compute bit, the largest power of 4 <= n
     let max_shift: u32 = T::zero().leading_zeros() - 1;
-    let shift: u32 = (max_shift - radicand.leading_zeros()) & !1;
+    let shift: u32 = (max_shift - radicand.leading_zeros()) & 0xFFFFFFFE; // make even
     let mut bit = T::one().checked_shl(shift)?;
 
     let mut n = radicand;
     let mut result = T::zero();
     while bit != T::zero() {
-        let result_with_bit = result.checked_add(&bit)?;
+        let result_with_bit = result | bit;
         if n >= result_with_bit {
             n = n.checked_sub(&result_with_bit)?;
-            result = result.checked_shr(1)?.checked_add(&bit)?;
+            result = result.checked_shr(1)? | bit;
         } else {
             result = result.checked_shr(1)?;
         }
