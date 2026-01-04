10ec081e8811f9a7e9ea673cda7521005e8ba2a8
GroovieGermanikus <grooviegermanikus@gmail.com>
2026-01-04 14:30:01 +0100

    re-export

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,11 @@
 
 extern crate core;
 
-pub mod approximations;
 pub mod checked_ceil_div;
 pub mod uint;
 
 pub mod precise_number;
+
+mod sqrt_math;
+mod distribution_math;
+pub mod approximations;
