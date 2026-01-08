5512cd1084ca3cf77886331e694e38c5c9aa9b9d
GroovieGermanikus <grooviegermanikus@gmail.com>
2026-01-08 08:27:52 +0100

    cleanup

--- src/uint.rs
+++ src/uint.rs
@@ -121,16 +121,3 @@ fn test_u256_checked_shl() {
     assert!(large.checked_shl(6).is_none());
 }
 
-
-#[inline(never)]
-pub fn sbf_can_do_bitshift(input: u128) -> u128 {
-    let input = U256::from(input);
-    let a= input >> 1;
-    a.as_u128()
-}
-
-#[inline(never)]
-pub fn sbf_can_do_div(input: u128) -> u128 {
-    let input = U256::from(input);
-    input.as_u128() / 3
-}
