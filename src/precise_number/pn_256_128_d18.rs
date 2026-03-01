1f7d93529fab915fdbcb3d73469e9a574854fd8e
GroovieGermanikus <grooviegermanikus@gmail.com>
2026-03-01 17:04:51 +0100

    tune sqrt precision

--- src/precise_number/pn_256_128_d18.rs
+++ src/precise_number/pn_256_128_d18.rs
@@ -6,8 +6,7 @@ use crate::{define_muldiv, define_precise_number, define_sqrt_tests};
 
 const ONE_CONST: U256 = U256([1000000000000000000, 0, 0, 0]);
 const ROUNDING_CORRECTION: U256 = U256([1000000000000000000 / 2, 0, 0, 0]);
-const PRECISION: U256 = U256([10000000000000000000,1000000000, 0, 0]); // TODO little-endian
-                                              // TODO
+const PRECISION: U256 = U256([10000000, 0, 0, 0]); // shoot for 11 digits
 const MAXIMUM_SQRT_BASE: U256 = U256([18446743073709551616, 18446744073709551615, 999999999999, 0]); // u128::MAX
 define_precise_number!(
     PreciseNumber,
@@ -38,6 +37,11 @@ mod tests {
     fn test_u256_rounding_correction_constant() {
         let rounding = U256::from(1_000000_000000_000000u128) / 2;
         assert_eq!(ROUNDING_CORRECTION, rounding);
+    }
+
+    #[test]
+    fn test_precision_constant() {
+        assert_eq!(format!("{}", PRECISION), "100");
     }
 
     #[test]
